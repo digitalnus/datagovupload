0ba66a038c541f34f51829700a8dca1c4b9ba805
Raspberry <common2332@gmail.com>
2022-04-15 12:31:46 +0800

    Initial Commit

---
+++ processdata.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+TODAY=`date +%Y%m%d`
+
+echo "Today is $TODAY"
+
+# Get zipped file from Github repo
+
+# Unzip raw data and put it into current date folder
+
+# Convert each xslx into csv file
+
+# Rename all column names by replacing space with _
+
+# Replace all empty column values with -
+
+# Upload processed data to Github
+
+
